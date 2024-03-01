cf5d3493049f790f704f85c958a38513766b3afd
Sriram Reddy <roarersri@gmail.com>
2024-03-01 18:38:41 -0500

    temp-2

--- Step - 2/index.js
+++ Step - 2/index.js
@@ -8,26 +8,26 @@ function updateMargin(){
     var content = document.getElementsByClassName("content")[0];
     content.style.marginLeft = courseMenu.getBoundingClientRect()["right"] + "px";
 }
-function hideButtons(){
-    console.log(123);
-    var screenWidth = window.innerWidth;
-    var buttons = document.getElementsByClassName("left-menu-section")[0];
-    var children = buttons.childNodes;
-    // console.log(children);
+// function hideButtons(){
+//     console.log(123);
+//     var screenWidth = window.innerWidth;
+//     var buttons = document.getElementsByClassName("left-menu-section")[0];
+//     var children = buttons.childNodes;
+//     // console.log(children);
 
-    for(var child of children){
-        if(child.tagName === "SPAN"){
-            console.log(child.getBoundingClientRect());
-            if(child.getBoundingClientRect()["right"]>screenWidth){
-                child.style.display = "none"
-            }else{
-                child.style.display = "block";
-            }
-        }
-    }
-    console.log(window.innerWidth);
-    console.log("---------------------------------------------");
-}
+//     for(var child of children){
+//         if(child.tagName === "SPAN"){
+//             console.log(child.getBoundingClientRect());
+//             if(child.getBoundingClientRect()["right"]>screenWidth){
+//                 child.style.display = "none"
+//             }else{
+//                 child.style.display = "block";
+//             }
+//         }
+//     }
+//     console.log(window.innerWidth);
+//     console.log("---------------------------------------------");
+// } // ->Not working
 window.addEventListener('load',updateMargin);
 window.addEventListener('resize',updateMargin);
-window.addEventListener("resize",hideButtons);//->Not a good idea and not working
+//window.addEventListener("resize",hideButtons);//->Not a good idea and not working
